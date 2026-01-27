a111eb576d0d4e21d4b932a6b19a7fecc10d659c
pubudu2003060 <madhushanpubudu2@gmail.com>
2026-01-27 09:57:28 +0530

    feat:load clickhouse from .env

--- captain/cmd/server/main.go
+++ captain/cmd/server/main.go
@@ -17,9 +17,9 @@ import (
 )
 
 func main() {
+	//select workspace and load environment variables
 	appEnv := os.Getenv("APP_ENV")
 	log.Println("APP_ENV:", appEnv)
-
 	if strings.ToLower(appEnv) == "dev" || strings.ToLower(appEnv) == "" {
 		if err := godotenv.Load(".env.dev"); err != nil {
 			log.Println("Error in loading .env.dev file:", err)
@@ -27,24 +27,29 @@ func main() {
 	} else {
 		log.Println("Running in production mode, using environment variables from Docker")
 	}
-
 	envConfig := config.Load()
 
+	//connect to postgres
 	pgConn, err := db.ConnectPostgres(envConfig.POSTGRES_URL)
 	if err != nil {
 		log.Fatal("Failed to init PostgresDB:", err)
 	}
 	defer pgConn.Close()
 
-	chConn, err := db.ConnectClickHouse(envConfig.CLICKHOUSE_URL)
+	//connect to clickhouse
+	chConn, err := db.ConnectClickHouse(envConfig.CLICKHOUSE_URL, envConfig.CLICKHOUSE_DB, envConfig.CLICKHOUSE_USER, envConfig.CLICKHOUSE_PASSWORD)
 	if err != nil {
 		log.Fatal("Failed to init ClickHouse:", err)
 	}
 	defer chConn.Close()
 
+	//create websocket manager
 	websocketManager := wsm.NewWebsocketManager()
+
+	//create router
 	router := server.NewRouter(pgConn, chConn, websocketManager)
 
+	//create server
 	srv := &http.Server{
 		Addr:         ":" + envConfig.PORT,
 		Handler:      router,
@@ -53,6 +58,7 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 
+	//start server
 	go func() {
 		log.Println("server running in port:", envConfig.PORT)
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -60,6 +66,7 @@ func main() {
 		}
 	}()
 
+	//graceful shutdown
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
 	<-stop
@@ -70,15 +77,12 @@ func main() {
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Printf("server shutdown error: %v", err)
 	}
-
 	log.Println("closing database connections...")
-
 	if pgConn != nil {
 		if err := pgConn.Close(); err != nil {
 			log.Printf("error closing Postgres: %v", err)
 		}
 	}
-
 	if chConn != nil {
 		if err := chConn.Close(); err != nil {
 			log.Printf("error closing ClickHouse: %v", err)
@@ -86,5 +90,4 @@ func main() {
 	}
 	websocketManager.Shutdown()
 	log.Println("server stopped")
-
 }
--- captain/internal/config/envConfig.go
+++ captain/internal/config/envConfig.go
@@ -7,21 +7,27 @@ import (
 )
 
 type Config struct {
-	PORT           string
-	POSTGRES_URL   string
-	Admin_API_KEY  string
-	Worker_API_KEY string
-	CLICKHOUSE_URL string
+	PORT                string
+	POSTGRES_URL        string
+	Admin_API_KEY       string
+	Worker_API_KEY      string
+	CLICKHOUSE_URL      string
+	CLICKHOUSE_DB       string
+	CLICKHOUSE_USER     string
+	CLICKHOUSE_PASSWORD string
 }
 
 func Load() Config {
 
 	config := Config{
-		PORT:           getEnv("PORT", "8080"),
-		POSTGRES_URL:   getEnv("POSTGRES_URL", ""),
-		Admin_API_KEY:  getEnv("ADMIN_API_KEY", ""),
-		Worker_API_KEY: getEnv("WORKER_API_KEY", ""),
-		CLICKHOUSE_URL: getEnv("CLICKHOUSE_URL", ""),
+		PORT:                getEnv("PORT", "8080"),
+		POSTGRES_URL:        getEnv("POSTGRES_URL", ""),
+		Admin_API_KEY:       getEnv("ADMIN_API_KEY", ""),
+		Worker_API_KEY:      getEnv("WORKER_API_KEY", ""),
+		CLICKHOUSE_URL:      getEnv("CLICKHOUSE_URL", ""),
+		CLICKHOUSE_DB:       getEnv("CLICKHOUSE_DB", ""),
+		CLICKHOUSE_USER:     getEnv("CLICKHOUSE_USER", ""),
+		CLICKHOUSE_PASSWORD: getEnv("CLICKHOUSE_PASSWORD", ""),
 	}
 
 	config.validate()
@@ -38,10 +44,13 @@ func getEnv(key, fallback string) string {
 
 func (c *Config) validate() {
 	required := map[string]string{
-		"POSTGRES_URL":   c.POSTGRES_URL,
-		"ADMIN_API_KEY":  c.Admin_API_KEY,
-		"WORKER_API_KEY": c.Worker_API_KEY,
-		"CLICKHOUSE_URL": c.CLICKHOUSE_URL,
+		"POSTGRES_URL":        c.POSTGRES_URL,
+		"ADMIN_API_KEY":       c.Admin_API_KEY,
+		"WORKER_API_KEY":      c.Worker_API_KEY,
+		"CLICKHOUSE_URL":      c.CLICKHOUSE_URL,
+		"CLICKHOUSE_DB":       c.CLICKHOUSE_DB,
+		"CLICKHOUSE_USER":     c.CLICKHOUSE_USER,
+		"CLICKHOUSE_PASSWORD": c.CLICKHOUSE_PASSWORD,
 	}
 
 	for key, val := range required {
